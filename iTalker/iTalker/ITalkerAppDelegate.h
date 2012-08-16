ded7615b9d544ad3df25bc410bcaebba9ab18c0d
tuyuanlin <eWalk_iOS@ZengmatoiMac.local>
2012-08-16 18:36:59 +0800

    init version

---
+++ iTalker/iTalker/ITalkerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ITalkerAppDelegate.h
+//  iTalker
+//
+//  Created by tuyuanlin on 12-8-16.
+//  Copyright (c) 2012年 cmcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITalkerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
